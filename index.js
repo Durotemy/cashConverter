7bf341a7f66b958370a9907eef3f094e7470c2fe
durotemy <durotemy@gmail.com>
2022-05-12 23:51:25 +0100

    cashConverter

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.getElementById("lbsInput").addEventListener("input", function (e) {
     document.getElementById("output").style.visibility = "visible";
     let lbs = e.target.value;
     document.getElementById("gramsOutput").innerHTML = lbs * 580;
-    document.getElementById("kgOutput").innerHTML = lbs * 1.23;
+    document.getElementById("kgOutput").innerHTML = lbs * 1.22;
     document.getElementById("ozOutput").innerHTML = lbs * 1.17;
 });
 
